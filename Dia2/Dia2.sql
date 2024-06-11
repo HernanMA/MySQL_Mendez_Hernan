b0dbe82bf946e4321d4db4518d262440fabc86c4
Hernan <156428836+HernanMA@users.noreply.github.com>
2024-06-11 09:12:37 -0500

    Add files via upload

--- Dia2/Dia2.sql
+++ Dia2/Dia2.sql
@@ -42,9 +42,47 @@ create table profesor(
     foreign key (id_departamento) references departamento(id)
 );
 
+-- Crear tabla de curso escolar
+create table curso (
+	id_curso int primary key,
+    año_inicio year(4) not null,
+    año_fin year(4) not null
+    );
+
+-- Crear tabla de grado
+create table grado (
+	id_grado int primary key,
+    nombre varchar (100) not null
+);
+
+-- Crear la tabla asignatura
+create table asignatura (
+	id_asignatura int primary key,
+    nombre varchar (100) not null,
+    creditos float not null,
+    id_grado int(10) not null,
+    tipo enum ('basica','obligatoria','optativa') not null,
+    curso tinyint (3) not null,
+    cuatrimestre tinyint (10) not null,
+    foreign key (id_asignatura) references profesor(id_profesor),
+    foreign key (id_grado) references grado(id_grado)
+);
+
+-- Crear tabla alumno se matricula en asignatura
+create table matricula (
+	id_alumno int primary key,
+    id_asignatura int (10) not null,
+    id_curso int (10) not null,
+    foreign key (id_alumno) references persona(id),
+    foreign key (id_asignatura) references asignatura(id_asignatura),
+    foreign key (id_curso) references curso(id_curso)
+);
 
 
 
+select * from grado;
+
+show tables;
 
 
 
